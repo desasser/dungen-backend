9a20a2fee867ce6bb191478dd62c24177d12b737
Daniel Sasser <desasser79@gmail.com>
2021-03-01 15:10:38 -0800

    round robin update

--- controllers/mapController.js
+++ controllers/mapController.js
@@ -6,6 +6,7 @@ const db = require("../models");
 
 const axios = require('axios');
 const { debugPort } = require("process");
+const e = require("express");
 
 require('dotenv').config()
 
@@ -26,7 +27,8 @@ router.get("/api/usermaps/:userId", function (req, res) {
   db.Map.findAll({
     where: {
       userId: req.params.userId
-    }
+    },
+    include: [db.MapTile]
   }).then((userMaps) => {
     res.json(userMaps)
   }).catch(error => {
@@ -97,11 +99,60 @@ router.delete("/api/deleteMap/:id", function (req, res) {
   })
 })
 
-router.get('/api/savemap', (req,res) => {
+router.post('/api/savemap', (req,res) => {
   db.Tile.findAll().then(data => {
-    mergeImg([data[0].image_url, data[1].image_url, data[2].image_url]).then(img => {
-      img.write('./assets/maps/output.png', ()=> console.log('done'))
+    const imgObjArr = [{
+      src: data[0].image_url,
+      offsetX: 0,
+      offsetY: 0
+    },
+    {
+      src: data[1].image_url,
+      offsetX: 0,
+      offsetY: 0
+    },
+    {
+      src: data[2].image_url,
+      offsetX: -398,
+      offsetY: 199
+    },
+    {
+      src: data[3].image_url,
+      offsetX: 0,
+      offsetY: 199
+    }
+  ]
+    
+    mergeImg(imgObjArr).then(img => {
+      img.write('./assets/maps/outputgrid.png', ()=> console.log('done'))
     })
+
+    // TODO: take in values from local storage
+    // TODO: confirm this syntax positions things properly
+    // let newRow = 0;
+    // // this should come from the table size
+    // const gridWidth = data.width;
+    // const imgObjArr = data.map(e => {
+    //   imgObjArr.src = e.image_url
+    //   if (newRow !== e.positionY) {
+    //     // if the newRow variable does not equal the y-position of the current element
+    //     // then the current element is on the next row
+    //     // set the first element in the row back to the start
+    //     // multiply -199 by the number of elements in the row
+    //     imgObjArr.offsetX = gridWidth * -199;
+    //   } else {
+    //     // if the newRow variable equals the y-position of the current element
+    //     // then the current element is on the same row as the last element
+    //     imgObjArr.offsetX = e.positionX*0;
+    //   }
+    //   imgObjArr.offsetY = e.positionY*199;
+    //   newRow = e.positionY
+    // });
+    // console.log(imageUrlArr);
+    // mergeImg(imgObjArr).then(img => {
+    //   img.write('./assets/maps/outputAll.png', ()=> console.log('done'))
+    // })
+    user_number
     res.json(data)
   })
 })
