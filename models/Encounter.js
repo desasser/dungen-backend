9a6a9a257849888512d7111f067b851bbfce85ee
Douglas Johnson <trunks22a@gmail.com>
2021-03-18 11:51:00 -0700

    issues resolved

--- models/Encounter.js
+++ models/Encounter.js
@@ -1,11 +1,12 @@
 module.exports = function (sequelize, DataTypes) {
     var Encounter = sequelize.define("Encounter", {
-        type: DataTypes.STRING,
         name: DataTypes.STRING,
         cr: DataTypes.STRING,
         dc: DataTypes.STRING,
         description: DataTypes.STRING
     });
-
+    Encounter.associate = function (models) {
+        Encounter.belongsTo(models.EncounterType)
+    };
     return Encounter;
 };
